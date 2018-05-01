6a3847f835c20e2c73675a5f4bd59440c58dcef2
xZero707 <xzero@elite7hackers.net>
2018-05-01 23:38:12 +0200

    Add missing file

---
+++ example.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace NorthernLights\Command\Example;
+
+use NorthernLights\Command\Result\CommandResultInterface;
+use function NorthernLights\Command\Run as command;
+
+require __DIR__ . '/vendor/autoload.php';
+
+/** @var CommandResultInterface $result */
+$result = command('ls 7rr -all');
+
+if ($result->isOk()) {
+    // Note: $result->getOutput() will return an instance of OutputInterface which implements __toString()
+    echo $result->getOutput() . PHP_EOL;
+} else {
+    echo "Command failed with status code " . $result->getExitCode() . PHP_EOL;
+}
